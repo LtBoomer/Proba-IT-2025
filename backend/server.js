56e6f98430dc76de4ce314396c8f0a537e635234
LtBoomer <vlad.boomer1@gmail.com>
2025-11-25 14:42:32 +0200

    Update to backend database, mongodb sync

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+mongoose.connect("mongodb+srv://admin:admin@pimp-your-grill.jgjtq6t.mongodb.net/")
+
+//app.get('/', (req, res) => {
+//  res.send('Hello World!')
+//})
+
+const schemaTest = new mongoose.Schema({
+  nume: String,
+  numar: Number
+});
+const testModel = mongoose.model('test', schemaTest);
+
+app.post('/', (req ,res) =>{
+  res.send("Something");
+  testModel.create({nume: 'blabla', numar: 15})
+})
+
+app.get('/testget', (req, res) =>{
+  res.send('macar asta merge')
 })
 
 app.listen(port, () => {
