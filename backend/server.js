5e2376c1b0e2ed10b5c4fc2c0b79cb936a33719d
LtBoomer <vlad.boomer1@gmail.com>
2025-11-25 17:14:51 +0200

    commit for get function, doesn't work

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
+
+app.use(express.json())
+let chestie = 'bla';
 mongoose.connect("mongodb+srv://admin:admin@pimp-your-grill.jgjtq6t.mongodb.net/")
 
 //app.get('/', (req, res) => {
@@ -15,15 +18,30 @@ const schemaTest = new mongoose.Schema({
 });
 const testModel = mongoose.model('test', schemaTest);
 
-app.post('/', (req ,res) =>{
-  res.send("Something");
-  testModel.create({nume: 'blabla', numar: 15})
+app.post('/testamchestii', (req ,res) =>{
+  const {nume, numar} = req.body
+  console.log(nume, numar)
+  if(nume != undefined && numar != undefined){
+  testModel.create({nume, numar})
+  res.send("da ba").status(200);
+  }else{
+    res.status(400).send("Empty stuff")
+  }
 })
 
 app.get('/testget', (req, res) =>{
   res.send('macar asta merge')
 })
 
+app.get('/testamchestii', (req,res) =>{
+  const data = testModel.find({name: "cevacoaie"})
+  console.log(data)
+})
+
+const functie = async () => {
+  const data = await testModel.find({nume: "uite ba scriu ceva"});
+}
+functie()
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
