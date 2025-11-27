0e9105d1ce7301b07b613ac42b226bf2522e8fdf
LtBoomer <vlad.boomer1@gmail.com>
2025-11-27 21:54:39 +0200

    Implemented fetch requests for app.jsx, implemented react-router-dom into main.jsx and cleaned up backend.js

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,13 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 
-let variable
 
-app.use(express.json())
-app.use(cors({
-  origin: "http://localhost:5173"
-}));
-mongoose.connect("mongodb+srv://admin:admin@pimp-your-grill.jgjtq6t.mongodb.net/")
+ app.use(cors({
+   origin: "http://localhost:5173"
+ }));
+ app.use(express.json());
+
+mongoose.connect("mongodb+srv://admin:admin@pimpgrill.w9atftp.mongodb.net/?appName=PimpGrill")
 
 const Schema = new mongoose.Schema({
   name: String,
@@ -19,12 +19,18 @@ const Schema = new mongoose.Schema({
 
 const test = mongoose.model("test", Schema)
 
+app.post('/post', (req, res) =>{
+  console.log(req.body.name, req.body.number)
+  test.create({name: req.body.name, number: req.body.number})
+  res.status(200).send("A mers")
+})
 
 app.get('/ceva', async (req, res) =>{
   const valoare = await test.find()
   console.log(valoare);
-  (res.send(valoare))
+  res.send(valoare).status(200);
 })
+
 
 
 app.listen(port, () => {
--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,19 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState([])
+  const [count, setCount] = useState({ name: "", number: 15 });
 
   return (
     <>
-    <button onClick={async (event) =>{
-      //setCount(fetch("http://localhost:3000/ceva"))
-      const variabilaAuxiliara = await fetch("https://localhost:3000/ceva");
-      setCount(variabilaAuxiliara)
-    }}>PushMeFucker</button>
-      </>
-  )
+      <textarea
+        onChange={(event) => {
+          setCount({
+            name: event.target.value,
+            number: count.number
+          })
+          console.log(event.target.value);
+        }}
+      ></textarea>
+      <button
+        onClick={async (event) => {
+          await fetch("http://localhost:3000/post", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name: count.name, number: count.number }),
+          });
+        }}
+      >
+        PushMeFucker
+      </button>
+      <button
+        onClick={async (event) => {
+          const url = "http://localhost:3000/ceva";
+          try {
+            const variabilaAuxiliara = await fetch(url);
+            const raspuns = await variabilaAuxiliara.json();
+            console.log(raspuns);
+          } catch (error) {
+            console.error(error.message);
+          }
+        }}
+      >
+        Push me for post
+      </button>
+    </>
+  );
 }
-export default App
+export default App;
